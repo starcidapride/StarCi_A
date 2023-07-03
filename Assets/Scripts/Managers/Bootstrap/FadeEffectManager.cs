f66ca275009341aed67e6ac35f44b62ffe637f76
Nguyễn Văn Tự Cường <cuongnvtse160875@gmail.com>
2023-07-04 02:50:52 +0700

    add fade

---
+++ Assets/Scripts/Managers/Bootstrap/FadeEffectManager.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeEffectManager : MonoBehaviour
+{
+    [SerializeField]
+    private Image transitionBackgroundImage;
+
+    [SerializeField]
+    [Range(0f, 5f)]
+    private float fadeDuration;
+
+    [SerializeField]
+    [Range(0f, 0.1f)]
+    private float fadeInterval;
+
+    public static bool beginLoad;
+
+    IEnumerator FadeAllEffect()
+    {
+        yield return StartCoroutine(FadeInEffect());
+
+        yield return new WaitForSeconds(1);
+
+        yield return StartCoroutine(FadeOutEffect());
+    }
+
+    private IEnumerator FadeInEffect()
+    {
+
+        transitionBackgroundImage.gameObject.SetActive(true);
+
+        var backgroundColor = transitionBackgroundImage.color;
+
+        backgroundColor.a = 0;
+
+        while (backgroundColor.a <= 1)
+        {
+            yield return new WaitForSeconds(fadeInterval);
+
+            backgroundColor.a += fadeInterval / fadeDuration;
+
+            transitionBackgroundImage.color = backgroundColor;
+        }
+        beginLoad = true;
+    }
+
+    private IEnumerator FadeOutEffect()
+    {
+        beginLoad = false;
+
+        var backgroundColor = transitionBackgroundImage.color;
+
+        while (backgroundColor.a >= 0)
+        {
+            yield return new WaitForSeconds(fadeInterval);
+
+            backgroundColor.a -= fadeInterval / fadeDuration;
+
+            transitionBackgroundImage.color = backgroundColor;
+        }
+
+        transitionBackgroundImage.gameObject.SetActive(false);
+
+    }
+
+    public void FadeIn()
+    {
+        StartCoroutine(FadeInEffect());
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(FadeOutEffect());
+    }
+
+    public void FadeAll()
+    {
+        StartCoroutine(FadeAllEffect());
+    }
+
+}
